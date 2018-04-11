54143fd7017efbf5de4971e91fc4818ced8b3490
Qiang <331616234@qq.com>
2018-04-11 18:33:27 +0800

    four page

--- deployserver/deploy.sh
+++ deployserver/deploy.sh
@@ -1,5 +1,5 @@
-kill -9 $(grep main)
-cd ~/goweb/
-git pull git@github.com:chuqiang61/goweb.git
-cd goweb
+#kill -9 $(grep main)
+#cd ~/goweb/
+#git pull git@github.com:chuqiang61/goweb.git
+#cd goweb
 ./main &
