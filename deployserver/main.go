dd568baa2ac756e0f33a3e5013ad66be1c5a2e92
Qiang <331616234@qq.com>
2018-04-11 16:53:01 +0800

    second page

---
+++ deployserver/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os/exec"
+	"log"
+)
+
+func reLaunch()  {
+	cmd :=exec.Command("sh","../deploy.sh")
+	err :=cmd.Start()
+	if err!=nil {
+		log.Fatal(err)
+	}
+	err = cmd.Wait()
+}
+
+func firstpage(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, " <h1>hello,this is my deploy server</h1>")
+	reLaunch()
+
+}
+
+func main() {
+	http.HandleFunc("/", firstpage)
+	http.ListenAndServe(":8000", nil)
+}
